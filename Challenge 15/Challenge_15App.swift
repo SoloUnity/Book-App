a436fc060f6edba66ffbd5b008603daf3470a37c
SoloUnity <GordonAccount@yahoo.com>
2022-06-20 17:22:36 -0400

    Initial Commit

---
+++ Challenge 15/Challenge_15App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_15App.swift
+//  Challenge 15
+//
+//  Created by Gordon Ng on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
